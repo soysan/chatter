6e0848af7f29e298fba5e35ab77442549d0b9827
Masayuki <soy2.san@gmail.com>
2024-05-06 18:06:22 +0900

    fix the way to read env

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
-    "os"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/soysan/chatter_n/server/pkg/db"
 	"github.com/soysan/chatter_n/server/pkg/handlers"
 	"github.com/soysan/chatter_n/server/pkg/middleware"
+	"os"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
+	// Render.com用の無駄 todo fix if they fixed
+	_, ok := os.LookupEnv("PRODUCTION_CHAT")
+	if !ok {
+		err := godotenv.Load()
+		if err != nil {
+			panic(err)
+		}
 	}
 
-	db := db.Init()
+	dbIns := db.Init()
 	// inject db to handlers
-	h := handlers.New(db)
+	h := handlers.New(dbIns)
 
 	router := gin.Default()
 	router.Use(cors.New(middleware.CorsSettings))
@@ -27,6 +31,9 @@ func main() {
 	router.POST("/chat", h.Chat)
 	router.GET("/history/list", h.HistoryList)
 
-    port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "6565"
+	}
 	router.Run(":" + port)
 }
