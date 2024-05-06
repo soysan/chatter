b8ff77c3f291e336ae9efaea2cf4a0bee8468662
Masayuki <soy2.san@gmail.com>
2024-05-06 20:03:46 +0900

    format

--- server/pkg/middleware/cors.go
+++ server/pkg/middleware/cors.go
@@ -2,32 +2,32 @@ package middleware
 
 import (
 	"github.com/gin-contrib/cors"
-	"time"
 	"os"
+	"time"
 )
 
-var origins []string
+func initCors() cors.Config {
+	var origins []string
 
-func InitCors() cors.Config {
-    if url := os.Getenv("FRONTEND_URL"); url != "" {
-        origins = append(origins, url)
-    }
+	if url := os.Getenv("FRONTEND_URL"); url != "" {
+		origins = append(origins, url)
+	}
 
-    origins = append(origins, "http://localhost:3000")
+	origins = append(origins, "http://localhost:3000")
 
-    return cors.Config{
-        AllowOrigins: origins,
-        AllowMethods: []string{
-            "GET",
-            "POST",
-        },
-        AllowHeaders: []string{
-            "Authorization",
-            "Content-Type",
-        },
-        AllowCredentials: true,
-        MaxAge:           24 * time.Hour,
-    }
+	return cors.Config{
+		AllowOrigins: origins,
+		AllowMethods: []string{
+			"GET",
+			"POST",
+		},
+		AllowHeaders: []string{
+			"Authorization",
+			"Content-Type",
+		},
+		AllowCredentials: true,
+		MaxAge:           24 * time.Hour,
+	}
 }
 
-var CorsSettings = InitCors()
+var CorsSettings = initCors()
